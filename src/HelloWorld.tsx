3a0bcf35b392884ad88c1ada2a072e7e973205d2
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2022-03-04 17:47:54 -0500

    💄 Change code style

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,13 +1,16 @@
+import { useVideoConfig } from "remotion";
+
 export const HelloWorld = () => {
+	const { fps, durationInFrames, width, height } = useVideoConfig();
 	return (
 		<div
 			style={{
 				flex: 1,
-				textAlign: 'center',
-				fontSize: '7em',
-				background: 'black',
-				color: 'white',
-				fontFamily: 'Nunito',
+				textAlign: "center",
+				fontSize: "7em",
+				background: "black",
+				color: "white",
+				fontFamily: "Nunito",
 			}}
 		>
 			Hello World!
